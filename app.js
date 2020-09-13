4bf08b85be85c8e99bce6faca3e227b95a37f50f
Pravesh Kunwar <praveshisawesome2017@gmail.com>
2020-09-12 22:51:38 -0400

    Updated major changes to code.

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ fetch("https://type.fit/api/quotes")
       ).innerHTML = `Quote: <i>${randomArray.text}</i>`;
       document.getElementById(`author`).innerHTML =
         randomArray.author || "<i>Unknown</i>";
+      const checkStr = "Unknown";
+      if (document.getElementById("author").innerText.includes(checkStr)) {
+        const grabDoc = document.getElementById("anchorTags");
+        return (grabDoc.innerHTML = "Couldn't search Unknown author.");
+      } else {
+        const grabDoc = document.getElementById("anchorTags");
+        grabDoc.setAttribute(
+          "href",
+          `https://www.google.com/search?q=${
+            document.getElementById("author").innerHTML
+          }`
+        );
+        grabDoc.setAttribute("target", "_blank");
+        grabDoc.innerHTML =
+          `Search for ${document.getElementById("author").innerHTML}` ||
+          "Can't search for unknown author";
+      }
     }
     buttonClick();
     document.getElementById("button").onclick = buttonClick;
